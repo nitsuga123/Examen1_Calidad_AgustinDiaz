d0c05c6804a26e89c2e7183c4e664740ae606000
LAPTOP-HCMQF8S9\Agustin <pepe_diaz92@hotmail.com>
2017-03-02 09:35:48 -0500

    codigo refactorizado

--- Tiles.cs
+++ Tiles.cs
@@ -1,39 +1,33 @@
 public class Tile
 {
-    private int n;
-    private int n2;
+ 
     private Tile up;
     private Tile down;
     private Tile right;
     private Tile left;
 
-    public class Tile (int n, int n2)
-    {
-        this.n = n;
-        this.n2 = n2;
-    }
 
-    public void Left (Tile tile)
+    public void SetleftTile (Tile tile)
     {
         left = tile;
     }
 
-    public void Right (Tile tile)
+    public void SetRightTile (Tile tile)
     {
         right = tile;
     }
 
-    public void Up (Tile tile)
+    public void SetUpTile (Tile tile)
     {
         up = tile;
     }
 
-    public void Down (Tile tile)
+    public void SetDownTile (Tile tile)
     {
         down = tile;
     }
 
-    public bool Check ()
+    public bool CheckCanMove()
     {
         return up != null;
     }
@@ -46,79 +40,72 @@ public class Tile
 
 public class TileMap
 {
-    private Tile [][] tiles;
+	private Tile [][] Board;
     public TileMap (int width, int height, int connections)
     {
-        tiles = new Tile[width][height];
+        Board = new Tile[width][height];
         for (int i=0; i<width; i++)
         {
             for (int j=0; i<height; j++)
             {
-                tiles[i][j] = new Tile (i, j);
+                Board[i][j] = new Tile (i, j);
             }
         }
-        int dx = 1;
-        int dy = 0;
+		int VariationPosx = 1;
+		int VariationPosy = 0;
         for (int i=0; i<connections; i++)
         {
-            int x = Random.Range (0, width-1);
-            int y = Random.Range (0, height-1);
-            Tile initial = tiles[x, y];
-            Tile tile = tiles[x+dx, y+dy];
-            if (dx == 1)
-                initial.Right (tile);
-            else if (dy == 1)
-                initial.Up (tile);
-            if (dx == 0)
-                dx = 1;
+			int RandomPosx = Random.Range (0, width-1);
+			int RandomPosy = Random.Range (0, height-1);
+            Tile initial = Board[RandomPosx, RandomPosy];
+            Tile tile = Board[RandomPosx+VariationPosx, RandomPosy+VariationPosy];
+            if (VariationPosx == 1)
+                initial.SetRightTile (tile);
+            else if (VariationPosy == 1)
+                initial.SetUpTile (tile);
+            if (VariationPosx == 0)
+                VariationPosx = 1;
             else
-                dx = 0;
-            if (dy == 0)
-                dy = 1;
+                VariationPosx = 0;
+            if (VariationPosy == 0)
+                VariationPosy = 1;
             else
-                dy = 0;
+                VariationPosy = 0;
         }
     }
 
     public Tile GetTile (int x, int y)
     {
-        return tiles[x, y];
+        return Board[x, y];
     }
 }
 
 public class Player
 {
-    private int x;
-    private int y;
+	private int Posx;
+	private int Posy;
     private TileMap map;
-
-    public class Player (TileMap map, int x, int y)
-    {
-        this.map = map;
-        this.x = x;
-        this.y = y;
-    }
 
     public void TryMove (Direction direction)
     {
-        Tile tile = map.GetTile (x, y);
+        Tile tile = map.GetTile (Posx, Posy);
         switch (direction)
         {
             case Direction.Left:
                 if (tile.CheckCanMove ())
-                    x -= 1;
+                    Posx -= 1;
                 break;
             case Direction.Right:
                 if (tile.CheckCanMove ())
-                    x += 1;
+                    Posx += 1;
                 break;
             case Direction.Up:
                 if (tile.CheckCanMove ())
-                    y += 1;
+                    Posy += 1;
                 break;
             case Direction.Down:
                 if (tile.CheckCanMove ())
-                    y -= 1;
+                    Posy -= 1;
                 break;
         }
     }
